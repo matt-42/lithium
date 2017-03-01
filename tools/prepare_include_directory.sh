2ca481fea93ce5fc24fde043a0b1415b89c4323a
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2017-03-01 13:05:38 +0100

    Initial commit.

---
+++ tools/prepare_include_directory.sh
@@ -0,0 +1,8 @@
+
+IODDIR=$1
+INCDIR=$2
+mkdir -p $INCDIR/iod
+
+[ -h $INCDIR/iod/metamap ]  || ln -sf $IODDIR/metamap/metamap $INCDIR/iod/metamap;
+[ -h $INCDIR/iod/symbol ]   || ln -sf $IODDIR/symbol/symbol $INCDIR/iod/symbol
+[ -h $INCDIR/iod/metajson ] || ln -sf $IODDIR/metajson/metajson $INCDIR/iod/metajson
