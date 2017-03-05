72f21918f56ba9d4861cc2a1be0d72bce2e06076
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2017-03-05 14:15:59 +0100

    Update.

--- tools/prepare_include_directory.sh
+++ tools/prepare_include_directory.sh
@@ -6,3 +6,4 @@ mkdir -p $INCDIR/iod
 [ -h $INCDIR/iod/metamap ]  || ln -sf $IODDIR/metamap/metamap $INCDIR/iod/metamap;
 [ -h $INCDIR/iod/symbol ]   || ln -sf $IODDIR/symbol/symbol $INCDIR/iod/symbol
 [ -h $INCDIR/iod/metajson ] || ln -sf $IODDIR/metajson/metajson $INCDIR/iod/metajson
+[ -h $INCDIR/iod/di ] || ln -sf $IODDIR/di/di $INCDIR/iod/di
