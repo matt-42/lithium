e85ac31afcd1a517d254a9d68ba6d37829e909e5
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2019-12-20 17:05:47 +0100

    Optimizations.

--- libraries/http_backend/http_backend/response.hh
+++ libraries/http_backend/http_backend/response.hh
@@ -32,8 +32,7 @@ struct http_response {
   }
 
   inline void write() { http_ctx.respond(body); }
-  //template <typename T> http_response& operator<<(T t) { http_ctx.respond(t); }
-  void set_status(int s) { http_ctx.set_status(s); }
+   void set_status(int s) { http_ctx.set_status(s); }
 
   template <typename A1, typename... A> inline void write(A1&& a1, A&&... a) {
     body += boost::lexical_cast<std::string>(std::forward<A1>(a1));
