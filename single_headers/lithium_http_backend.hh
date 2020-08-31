215bcaf44429ea0f5ebd336aed66530c5e564907
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2020-08-31 20:21:34 +0200

    Single headers.

--- single_headers/lithium_http_backend.hh
+++ single_headers/lithium_http_backend.hh
@@ -5731,6 +5731,47 @@ auto sql_crud_api(sql_orm_schema<A, B, C>& orm_schema) {
 
 #endif // LITHIUM_SINGLE_HEADER_GUARD_LI_HTTP_BACKEND_SQL_CRUD_API_HH
 
+#ifndef LITHIUM_SINGLE_HEADER_GUARD_LI_HTTP_BACKEND_GROWING_OUTPUT_BUFFER_HH
+#define LITHIUM_SINGLE_HEADER_GUARD_LI_HTTP_BACKEND_GROWING_OUTPUT_BUFFER_HH
+
+
+namespace li {
+
+template <int CHUNK_SIZE = 2000>
+struct growing_output_buffer {
+
+  growing_output_buffer() :
+    buffer_(sbo_, CHUNK_SIZE, [this] (const char* s, int size) {
+      // append s to the growing buffer.
+      int old_growing_size = growing_.size();
+      growing_.resize(growing_.size() + size);
+      memcpy(growing_.data() + old_growing_size, s, size);
+    }) {
+  }
+
+  void reset() { growing_.clear(); buffer_.reset(); }
+  std::size_t size() { return buffer_.size() + growing_.size(); }
+
+  std::string_view to_string_view() {
+    if (growing_.size() == 0) return buffer_.to_string_view();
+    else {
+      buffer_.flush();
+      return std::string_view(growing_);
+    }
+  }
+
+  template <typename T>
+  growing_output_buffer& operator<<(T&& s) { buffer_ << s; }
+
+  char sbo_[CHUNK_SIZE];
+  std::string growing_;
+  output_buffer buffer_;
+};
+
+}
+
+#endif // LITHIUM_SINGLE_HEADER_GUARD_LI_HTTP_BACKEND_GROWING_OUTPUT_BUFFER_HH
+
 
 #ifndef LITHIUM_SINGLE_HEADER_GUARD_LI_HTTP_BACKEND_HTTP_BENCHMARK_HH
 #define LITHIUM_SINGLE_HEADER_GUARD_LI_HTTP_BACKEND_HTTP_BENCHMARK_HH
