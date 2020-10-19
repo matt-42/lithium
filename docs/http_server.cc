908bb7639c3d6be086b2ac0bbc9c33b41d3cf9da
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2020-10-19 22:49:10 +0200

    More doc.

--- docs/http_server.cc
+++ docs/http_server.cc
@@ -1,69 +1,69 @@
+#include "lithium_http_server.hh"
+
+int main() {
+
 // __documentation_starts_here__
 /*
 http_server
 =================================
 
-This library goal is to ease the development of HTTP APIs without
-compromising on performance. Coroutines (based on epoll and boost::context) 
-are used to provide a asynchronous execution without impacting the ease
-of use : Only few threads can serve thousands of clients.
+## Introduction
 
-All the communication asynchronous : client <-> server communication and
-also server <-> database (except SQLite which does not use the network).
+### Motivations
 
+The goal of this library is to take advangage of C++17 new features
+to simplify the writing of high performance web server. It provides consise
+and simple construcs that allows non C++ experts to quickly implement simple HTTP
+servers.
 
-## Main features
-  - High performance aync input/output engine based on epoll
-  - PostgreSQL (async), Mysql (async), Sqlite (sync) connectors
-  - Authentication
-  - Object relational mapping for SQL databases
-  - Static file serving
-  - HTTP Pipelining
-  - HTTPS
-
-## Dependencies
+### Dependencies
   - OpenSSL
   - Boost
 
-## How to write an api
-*/
-// hello_lithium.cc
+### Hello World
+
+Write your first hello world server:
+```c++
+// hello_world.cc
 #include "lithium_http_server.hh"
 
-using namespace li;
-
 int main() {
-  // Build an api.
-  http_api api;
 
-  // Define a HTTP GET endpoint.
-  api.get("/hello_world") = [&](http_request& request, http_response& response) {
+  // Define an api with one HTTP GET route.
+  li::http_api api;
+  api.get("/hello_world") = [&](li::http_request& request, li::http_response& response) {
     response.write("hello world.");
   };
 
   // Start a http server.
-  http_serve(api, 12345);
+  li::http_serve(api, 12345);
 
-  // Or start a https server:
-  http_serve(api, 443, s::ssl_key = "./server.key", s::ssl_certificate = "./server.crt");
-
-  // Optionally start a https with your ciphersuite
-  http_serve(api, 443, s::ssl_key = "./server.key", s::ssl_certificate = "./server.crt", s::ssl_ciphers = "ALL:!NULL");
 }
+```
+
+Run it with the cli:
+```bash
+$ li run ./hello_world.cc
+```
 /*
 
-## Metamaps
+## Error handling
 
-If you are new to lithium, please read this to make sure you understand the metamap paradigm:
-https://github.com/matt-42/lithium/tree/master/libraries/metamap
+`li::http_server` uses exceptions to report HTTP errors.
 
-## Request parameters
+*/
+http_api api;
+api.get("/unauthorized") = [&](http_request& request, http_response& response) {
+  throw http_error::unauthorized("You cannot access this route.");
+};
+/*
+## GET / POST / URL request parameters
+
+`request.get_parameters`, `request.post_parameters` and `request.url_parameters` allows to read request parameters.
 */
 http_api api;
 api.get("/get_params") = [&](http_request& request, http_response& response) {
   // This will throw a BAD REQUEST http error if one field is missing or ill-formated.
-  // Note: all s::XXXX are symbols, they must be defined, check here for more info:
-  // https://github.com/matt-42/lithium/tree/master/libraries/metamap
   auto params = request.get_parameters(s::my_param = int(), s::my_param2 = std::string());
   response.write("hello " + params.my_param2);
 };
@@ -78,8 +78,11 @@ api.post("/url_params/{{name}}") = [&](http_request& request, http_response& res
   auto params = request.url_parameters(s::name = std::string());
   response.write("hello " + params.name);
 };
+/*
+## Optional request parameters
 
-// You can also pass optional parameters:
+You can also take optional parameters:
+*/
 auto param = request.get_parameters(s::my_param = std::optional<int>());
 // here param.id has type std::optional<int>()
 if (param.id)
@@ -88,135 +91,53 @@ else
   std::cout << "optional set: " << param.id.value() >> << std::endl;
 /*
 
-## Error handling
+## Headers and cookies
 
-`li::http_server` uses exceptions to report HTTP errors.
-
-*/
-http_api api;
-api.get("/unauthorized") = [&](http_request& request, http_response& response) {
-  throw http_error::unauthorized("You cannot access this route.");
-};
-/*
-
-## Reading and writing headers and cookies
-
+Headers and cookies are accessed using `request.header` and `request.cookie`:
 */
 
 api.get("/unauthorized") = [&](http_request& request, http_response& response) {
   const char* value = request.header("_header_name_");
   const char* value = request.cookie("_cookie_name_");
-  // Values are null if the header/cookie does not exists.
+  // Values are nullptr if the header/cookie does not exists.
 
   response.set_header("header_name", "header value");
   response.set_cookie("cookie_name", "cookie_value");
 };
 
 /*
+## Nested APIs
 
-## SQL Databases
-
-The sqlite, mysql, postgresql single headers library are located here:
-https://github.com/matt-42/lithium/tree/master/single_headers
-
-Download the one suited for your project and /*#include/* it in your code.
+APIs can be nested so its trivial to plug one API into a larger one.
 
 */
-// Declare a sqlite database.
-auto database = sqlite_database("iod_sqlite_test_orm.db");
 
-// Or a mysql database.
-auto database = mysql_database(s::host = "127.0.0.1",
-                              s::database = "silicon_test",
-                              s::user = "root",
-                              s::password = "sl_test_password",
-                              s::port = 14550,
-                              s::charset = "utf8");
-
-api.post("/db_test") = [&] (http_request& request, http_response& response) {
-  auto connection = database.connect();
-  int count = connection("Select count(*) from users").read<int>();
-  std::optional<std::string> name = 
-    connection("Select name from users LIMIT 1").read_optional<std::string>();
-  if (name) response.write(*name);
-  else response.write("empty table");
-};
+http_api root_api;
+http_api nested_api;
+// Let's assume there is something in these two APIs.
+root_api.add_subapi("/prefix_of_the_nested_api", nested_api);
 
 /*
-
-Check https://github.com/matt-42/lithium/tree/master/libraries/sql for more information
-on how to use these connections.
-
-## Object Relational Mapping
-
-*/
-auto users = li::sql_orm_schema(database, "user_table" /* the table name in the SQL db*/)
-
-              .fields(s::id(s::auto_increment, s::primary_key) = int(),
-                      s::age = int(),
-                      s::name = std::string(),
-                      s::login = std::string());
-
-api.post("/orm_test") = [&] (http_request& request, http_response& response) {
-  auto U = users.connect();
-  long long int id = U.insert(s::name = "john", s::age = 42, s::login = "doe");
-  response.write("new inserted id: ", id, ", new user count is ", U.count());
-  
-  U.update(s::id = id, s::age = 43);
-  assert(U.find_one(s::id = id)->age == 43);
-  U.remove(s::id = id);
-
-}
-/*
-### ORM Callbacks
-
-If you need to insert custom logic in the ORM, you can pass callbacks to the orm:
-*/
-auto users = li::sql_orm_schema(database, "user_table")
-              .fields([...])
-              .callbacks(s::before_insert = [] (auto& user) { ... });
-/*
-
-Callbacks can also take additional arguments:
-*/
-auto users = li::sql_orm_schema(database, "user_table")
-              .fields([...])
-              .callbacks(
-                s::before_insert = [] (auto& user, http_request& request) { ... });
-
-// Additional arguments are passed to the ORM methods:
-api.post("/orm_test") = [&] (http_request& request, http_response& response) {
-  users.connect().insert(s::name = "john", s::age = 42, s::login = "doe", request);
-}
-/*
-
-
-More info on the ORM here: https://github.com/matt-42/lithium/tree/master/libraries/sql
-
-## Insert/Update/Remove API
-
-If you are lazy to write update/remove/remove/find_by_id routes for all you objects, you can
-use `sql_crud_api` to define them automatically 
-*/
-api.add_subapi("/user", sql_crud_api(my_orm));
-/*
-Note that when using it, ORM callbacks must take `http_request& request, http_response& response`
-as additional arguments.
-
-Check the code for more info:
-https://github.com/matt-42/lithium/blob/master/libraries/http_server/http_server/sql_crud_api.hh
-
 
 ## Sessions
 
-*/
-// Let's use a session to remember the name of a user.
-// By default, we set the username to "unknown"
-// Store sessions in a sql db:
-auto session = sql_http_session(db, "user_sessions_table", "test_cookie", s::name = "unknown");
-// Or in-memory (does not persist after a server shutdown).
-auto session = hashmap_http_session("test_cookie", s::name = "unknown");
+Let's use a session to remember the name of a user.
 
+To store the session in the server memory, use `hashmap_http_session`. In this example,
+we'll store the name of the current user. When the session is new, the name will be initialized
+to `'unknown'`.
+*/
+auto session = hashmap_http_session("session_cookie_name", s::name = "unknown");
+/*
+To store in in a database, use `sql_http_session`:
+*/
+auto db = sqlite_database("session_database.sqlite");
+auto sql_session = sql_http_session(db, "user_sessions_table", "session_cookie_name", s::name = "unknown");
+/*
+
+Session can then be accessed in a handler using the `connect` method of the global session object.
+It returns an object with the session fields:
+*/
 api.get("/sessions") = [&] (http_request& request, http_response& response) {
   auto sess = session.connect(request, response);
 
@@ -234,7 +155,13 @@ api.get("/sessions") = [&] (http_request& request, http_response& response) {
 
 ## User authentication
 
+On top of SQL sessions is built `http_authentication` that allows
+to authenticate users. It requires to instantiate several object fisrt
+  - a SQL database so we know where is the user table (including login and password), and where to store the sessions
+  - a user ORM so we know the structure of user table
+  - a session object so we can remember a logged user.
 */
+
 auto db = sqlite_database("blog_database.sqlite");
 auto users =
     li::sql_orm_schema(db, "auth_users")
@@ -245,10 +172,7 @@ auto users =
 // Store user session in sql:
 auto sessions = sql_http_session(db, "user_sessions_table", "test_cookie", s::name = "unknown");
 
-// Or in memory:
-auto sessions = li::hashmap_http_session("auth_cookie", s::user_id = -1);
-
-//
+// Authenticator 
 auto auth = li::http_authentication(// the sessions to store the user id of the logged user.
                                     // It need to have a user_id field.
                                     sessions,
@@ -265,10 +189,18 @@ auto auth = li::http_authentication(// the sessions to store the user id of the 
                                       },
                                     );
 
+/*
+Once the authenticator build we can use it in the handlers.
+
+3 methods are available:
+  - auth.current_user(request, response) -> std::optional<user_type>: 
+      Return the current user object as it is stored in the database. If no user is logged, 
+      return an empty std::optional.
+*/
 li::api<li::http_request, li::http_response> my_api;
 
 my_api.post("/auth_test") = [&] (http_request& request, http_response& response) {
-  // Login: 
+  // auth.login: 
   //   Read the email/password field in the POST parameters
   //   Check if this match with a user in DB.
   //   Store the user_id in the session.
@@ -308,3 +240,5 @@ assert(r.body == "expected response body");
 Checkout the code here:
 https://github.com/matt-42/lithium/blob/master/libraries/http_server/examples/blog.cc
 */
+
+}
