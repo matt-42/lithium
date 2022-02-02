0f0afb2edc0f52a5eddaac0e8799b15f425bf9fe
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2022-02-02 19:23:51 +0100

    Fix mime types

--- test/linux/run_clang.sh
+++ test/linux/run_clang.sh
@@ -2,7 +2,8 @@
 
 docker run -it -v $HOME/lithium:/lithium lithium_linux_test /bin/bash -c "
   /lithium/test/linux/setup_databases.sh && 
-  mkdir -p /tmp/build && cd /tmp/build && 
-  ls /lithium && 
-  cmake -DCMAKE_CXX_COMPILER=clang++-9 /lithium && 
+  git clone /lithium /tmp/lithium && cd /tmp/lithium &&
+  ./vcpkg/bootstrap-vcpkg.sh &&
+  ./vcpkg/vcpkg install &&
+  cmake -DCMAKE_CXX_COMPILER=clang++-9 . && 
   make -j12 && ctest"
