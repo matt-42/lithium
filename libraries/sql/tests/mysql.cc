6c413e6416a4c8fe559b4685134103056d56537a
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2022-02-04 12:18:07 +0100

    Mysql debug.

--- libraries/sql/tests/mysql.cc
+++ libraries/sql/tests/mysql.cc
@@ -13,7 +13,11 @@ int main() {
       mysql_database(s::host = "127.0.0.1", s::database = "mysql_test", s::user = "root",
                      s::password = "lithium_test", s::port = 14550, s::charset = "utf8");
 
-  generic_sql_tests(database);
+
+  auto query = database.connect();
+
+  // generic_sql_tests(database);
+  assert(query("SELECT 1 + 2").read<int>() == 3);
 
   database.clear_connections();
 }
